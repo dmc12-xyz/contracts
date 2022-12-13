204d2031f1427d389ec8cc4f66a59a85bf70879b
atul <atul.patare@launchventures.co>
2022-12-13 15:22:54 +0530

    added tests for meta txs

--- meta-transactions/test/meta.ts
+++ meta-transactions/test/meta.ts
@@ -1,124 +1,182 @@
-import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
-import { expect } from "chai";
-import { ethers } from "hardhat";
+const { expect } = require("chai");
+const { BigNumber } = require("ethers");
+const { arrayify, parseEther } = require("ethers/lib/utils");
+const { ethers } = require("hardhat");
 
-describe("Lock", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
+describe("MetaTokenTransfer", function () {
+  it("Should let user transfer tokens through a relayer with different nonces", async function () {
+    // Deploy the contracts
+    const RandomTokenFactory = await ethers.getContractFactory("RandomToken");
+    const randomTokenContract = await RandomTokenFactory.deploy();
+    await randomTokenContract.deployed();
 
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+    const MetaTokenSenderFactory = await ethers.getContractFactory(
+        "TokenSender"
+    );
+    const tokenSenderContract = await MetaTokenSenderFactory.deploy();
+    await tokenSenderContract.deployed();
 
-    // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
+    // Get three addresses, treat one as the user address
+    // one as the relayer address, and one as a recipient address
+    const [_, userAddress, relayerAddress, recipientAddress] =
+        await ethers.getSigners();
 
-    const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    // Mint 10,000 tokens to user address (for testing)
+    const tenThousandTokensWithDecimals = parseEther("10000");
+    const userTokenContractInstance = randomTokenContract.connect(userAddress);
+    const mintTxn = await userTokenContractInstance.freeMint(
+        tenThousandTokensWithDecimals
+    );
+    await mintTxn.wait();
 
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
-  }
+    // Have user infinite approve the token sender contract for transferring 'RandomToken'
+    const approveTxn = await userTokenContractInstance.approve(
+        tokenSenderContract.address,
+        BigNumber.from(
+            // This is uint256's max value (2^256 - 1) in hex
+            "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+        )
+    );
+    await approveTxn.wait();
 
-  describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
+    // Have user sign message to transfer 10 tokens to recipient
+    let nonce = 1;
 
-      expect(await lock.unlockTime()).to.equal(unlockTime);
-    });
+    const transferAmountOfTokens = parseEther("10");
+    const messageHash = await tokenSenderContract.getHash(
+        userAddress.address,
+        transferAmountOfTokens,
+        recipientAddress.address,
+        randomTokenContract.address,
+        nonce
+    );
+    const signature = await userAddress.signMessage(arrayify(messageHash));
 
-    it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
+    // Have the relayer execute the transaction on behalf of the user
+    const relayerSenderContractInstance =
+        tokenSenderContract.connect(relayerAddress);
+    const metaTxn = await relayerSenderContractInstance.transfer(
+        userAddress.address,
+        transferAmountOfTokens,
+        recipientAddress.address,
+        randomTokenContract.address,
+        nonce,
+        signature
+    );
+    await metaTxn.wait();
 
-      expect(await lock.owner()).to.equal(owner.address);
-    });
+    // Check the user's balance decreased, and recipient got 10 tokens
+    let userBalance = await randomTokenContract.balanceOf(userAddress.address);
+    let recipientBalance = await randomTokenContract.balanceOf(
+        recipientAddress.address
+    );
 
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
+    expect(userBalance.eq(parseEther("9990"))).to.be.true;
+    expect(recipientBalance.eq(parseEther("10"))).to.be.true;
 
-      expect(await ethers.provider.getBalance(lock.address)).to.equal(
-        lockedAmount
-      );
-    });
+    // Increment the nonce
+    nonce++;
 
-    it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future"
-      );
-    });
+    // Have user sign a second message, with a different nonce, to transfer 10 more tokens
+    const messageHash2 = await tokenSenderContract.getHash(
+        userAddress.address,
+        transferAmountOfTokens,
+        recipientAddress.address,
+        randomTokenContract.address,
+        nonce
+    );
+    const signature2 = await userAddress.signMessage(arrayify(messageHash2));
+    // Have the relayer execute the transaction on behalf of the user
+    const metaTxn2 = await relayerSenderContractInstance.transfer(
+        userAddress.address,
+        transferAmountOfTokens,
+        recipientAddress.address,
+        randomTokenContract.address,
+        nonce,
+        signature2
+    );
+    await metaTxn2.wait();
+
+    // Check the user's balance decreased, and recipient got 10 tokens
+    userBalance = await randomTokenContract.balanceOf(userAddress.address);
+    recipientBalance = await randomTokenContract.balanceOf(
+        recipientAddress.address
+    );
+
+    expect(userBalance.eq(parseEther("9980"))).to.be.true;
+    expect(recipientBalance.eq(parseEther("20"))).to.be.true;
   });
 
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called too soon", async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
+  it("Should not let signature replay happen", async function () {
+    // Deploy the contracts
+    const RandomTokenFactory = await ethers.getContractFactory("RandomToken");
+    const randomTokenContract = await RandomTokenFactory.deploy();
+    await randomTokenContract.deployed();
 
-        await expect(lock.withdraw()).to.be.revertedWith(
-          "You can't withdraw yet"
-        );
-      });
+    const MetaTokenSenderFactory = await ethers.getContractFactory(
+        "TokenSender"
+    );
+    const tokenSenderContract = await MetaTokenSenderFactory.deploy();
+    await tokenSenderContract.deployed();
 
-      it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
+    // Get three addresses, treat one as the user address
+    // one as the relayer address, and one as a recipient address
+    const [_, userAddress, relayerAddress, recipientAddress] =
+        await ethers.getSigners();
 
-        // We can increase the time in Hardhat Network
-        await time.increaseTo(unlockTime);
+    // Mint 10,000 tokens to user address (for testing)
+    const tenThousandTokensWithDecimals = parseEther("10000");
+    const userTokenContractInstance = randomTokenContract.connect(userAddress);
+    const mintTxn = await userTokenContractInstance.freeMint(
+        tenThousandTokensWithDecimals
+    );
+    await mintTxn.wait();
 
-        // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-          "You aren't the owner"
-        );
-      });
+    // Have user infinite approve the token sender contract for transferring 'RandomToken'
+    const approveTxn = await userTokenContractInstance.approve(
+        tokenSenderContract.address,
+        BigNumber.from(
+            // This is uint256's max value (2^256 - 1) in hex
+            "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+        )
+    );
+    await approveTxn.wait();
 
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture
-        );
+    // Have user sign message to transfer 10 tokens to recipient
+    let nonce = 1;
 
-        // Transactions are sent using the first signer by default
-        await time.increaseTo(unlockTime);
+    const transferAmountOfTokens = parseEther("10");
+    const messageHash = await tokenSenderContract.getHash(
+        userAddress.address,
+        transferAmountOfTokens,
+        recipientAddress.address,
+        randomTokenContract.address,
+        nonce
+    );
+    const signature = await userAddress.signMessage(arrayify(messageHash));
 
-        await expect(lock.withdraw()).not.to.be.reverted;
-      });
-    });
+    // Have the relayer execute the transaction on behalf of the user
+    const relayerSenderContractInstance =
+        tokenSenderContract.connect(relayerAddress);
+    const metaTxn = await relayerSenderContractInstance.transfer(
+        userAddress.address,
+        transferAmountOfTokens,
+        recipientAddress.address,
+        randomTokenContract.address,
+        nonce,
+        signature
+    );
+    await metaTxn.wait();
 
-    describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw())
-          .to.emit(lock, "Withdrawal")
-          .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-      });
-    });
-
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
-          [lockedAmount, -lockedAmount]
-        );
-      });
-    });
+    // Have the relayer attempt to execute the same transaction again with the same signature
+    // This time, we expect the transaction to be reverted because the signature has already been used.
+    expect(relayerSenderContractInstance.transfer(
+        userAddress.address,
+        transferAmountOfTokens,
+        recipientAddress.address,
+        randomTokenContract.address,
+        nonce,
+        signature
+    )).to.be.revertedWith('Already executed!');
   });
 });
