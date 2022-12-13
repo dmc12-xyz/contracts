313acd57789064ae77206351cc2f3292461f6309
atul <atul.patare@launchventures.co>
2022-12-13 16:03:20 +0530

    updated config for hardhat

--- mev/hardhat.config.ts
+++ mev/hardhat.config.ts
@@ -1,8 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+
+const RPC_HTTP_URL = process.env.RPC_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: RPC_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  }
 };
 
 export default config;
